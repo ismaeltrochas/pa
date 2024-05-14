6b288e19a9ff6b619b241e4a82e2b459214aaca6
Ismael Trocha <ismaeltrocha@gmail.com>
2024-05-13 22:22:30 -0500

    first commit

---
+++ gateway/src/test/java/com/fastlearn/gateway/FlGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastlearn.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
