6df5f666d3899745a7af0ef28c1787cff273e163
ismael.trocha <ismaeltrocha@gmail.com>
2024-05-17 17:33:45 -0500

    Avanzando el microservicio de usuaris para el registro de los mismos.
    TODO: Configuración de Keycloak, Crear usuarios por medio de end points, crear roles por medio de la cli.

--- users/src/main/java/com/fastlearn/users/infrastructure/configuration/keycloak/KeycloakProvider.java
+++ users/src/main/java/com/fastlearn/users/infrastructure/configuration/keycloak/KeycloakProvider.java
@@ -1,4 +1,46 @@
 package com.fastlearn.users.infrastructure.configuration.keycloak;
 
+import lombok.Getter;
+import org.jboss.resteasy.client.jaxrs.internal.ResteasyClientBuilderImpl;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.UsersResource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "keycloak")
+@Getter
 public class KeycloakProvider {
+
+    private static String serverUrl;
+    private static String realmName;
+    private static String realmMaster;
+    private static String adminCli;
+    private static String userConsole;
+    private static String passwordConsole;
+    private static String clientSecret;
+
+
+    public static RealmResource getRealmResource() {
+        Keycloak keycloak = KeycloakBuilder.builder()
+                .serverUrl(serverUrl)
+                .realm(realmName)
+                .clientId(adminCli)
+                .username(userConsole)
+                .password(passwordConsole)
+                .clientSecret(clientSecret)
+                .resteasyClient(new ResteasyClientBuilderImpl()
+                        .connectionPoolSize(10)
+                        .build())
+                .build();
+
+        return keycloak.realm(realmName);
+    }
+
+    public static UsersResource userResource() {
+        RealmResource realmResource = getRealmResource();
+        return realmResource.users();
+    }
 }
