1f217b0117d78cc81f626864feb6163868a428d3
ismael.trocha <ismaeltrocha@gmail.com>
2024-05-17 11:03:27 -0500

    Agregando proyecto para el registro de usuarios utilizando Keycloak como proveedor de IAM

---
+++ users/src/test/java/com/fastlearn/users/UsersApplicationTests.java
@@ -0,0 +1,12 @@
+package com.fastlearn.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+ 	void contextLoads() {
+	}
+
+}
